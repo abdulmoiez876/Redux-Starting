6c4fec52f4149acf18a3701411a22c337db23b87
abdulmoiez876 <abdul.moiez876@gmail.com>
2023-01-31 23:55:57 +0500

    auth slice done

--- src/store/index.js
+++ src/store/index.js
@@ -25,8 +25,26 @@ const counterSlice = createSlice({
     }
 })
 
+const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        authenticated: false
+    },
+    reducers: {
+        login(state) {
+            state.authenticated = true
+        },
+        logout(state) {
+            state.authenticated = false
+        }
+    }
+})
+
 const store = configureStore({
-    reducer: counterSlice.reducer
+    reducer: {
+        counter: counterSlice.reducer,
+        auth: authSlice.reducer
+    }
 });
 
 export const counterActions = counterSlice.actions;
